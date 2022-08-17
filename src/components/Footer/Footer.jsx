b06589e6c713c9e4ad32b8bb4c9bd840059bf658
Digoassun <digoassun@gmail.com>
2022-08-17 18:00:00 -0300

    Ultima sessão mais footer finalizado

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React, { StrictMode } from "react";
+import S from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={S.footer}>
+      <p className={S.p}>Testando suas habilidades em HTML, CSS e JS.</p>
+      <p className={S.p}>Linx Impulse</p>
+      <p className={S.p}>2019</p>
+    </footer>
+  );
+};
+
+export default Footer;
