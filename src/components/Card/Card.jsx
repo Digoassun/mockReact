731fe2bb0df32efa9f05c9ca908a523526103da9
Digoassun <digoassun@gmail.com>
2022-08-18 23:54:22 -0300

    Adicionada requisição da API de produtos

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,16 +1,18 @@
-import React, { StrictMode } from "react";
+import React from "react";
 import Button from "../Button/Button";
 import S from "./Card.module.css";
 
-const Card = ({ nome, desc, de, por, ou }) => {
+const Card = ({ img,nome, desc, de, por, vezes, ou }) => {
   return (
     <article className={S.card} >
-      <div className={S.img}></div>
+      <picture className={S.img}>
+        <img src={img}  alt="produto" />
+      </picture>
       <h3 className={S.h3}>{nome}</h3>
-      <p className={S.desc}>{desc}</p>
-      <h4 className={S.h4}>De:{de}</h4>
-      <h5 className={S.h5}>Por:{por}</h5>
-      <p className={S.vezes}>ou 2x de:{ou}</p>
+      <small className={S.desc}>{desc}</small>
+      <h4 className={S.h4}>De:{de},00</h4>
+      <h5 className={S.h5}>Por:{por},00</h5>
+      <p className={S.vezes}>ou {vezes}x de:{ou}</p>
       <Button style={S.button} content="Comprar" />
     </article>
   );
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,4 +1,5 @@
-import React, { StrictMode } from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 import Button from "../Button/Button";
 import Card from "../Card/Card";
 import Form from "../Form/Form";
@@ -7,6 +8,26 @@ import PresText from "../PresText/PresText";
 import S from "./Main.module.css";
 
 const Main = () => {
+  const [informacoes, setInformacoes] = useState([]);
+  const [page, setPage] = useState(1)
+
+  async function handleRequisicao() {
+    const response = await fetch(
+      `https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=${page}`
+    );
+    const json = await response.json();
+    setInformacoes([...informacoes,...json.products]);
+    console.log(informacoes)
+  }
+
+  const nextPage = ()=>{
+    setPage(page+ 1)
+  }
+
+  useEffect(() => {
+    handleRequisicao();
+  }, [page]);
+
   return (
     <main className={S.main}>
       <section className={S.firstSec}>
@@ -24,64 +45,23 @@ const Main = () => {
           </div>
         </div>
         <div className={S.cards}>
-          <Card
-            nome="nome do produto"
-            desc="descrição completa do produto aqui "
-            de="10,99"
-            por="5,99"
-            ou="6,99"
-          />
-          <Card
-            nome="nome do produto"
-            desc="descrição completa do produto aqui "
-            de="10,99"
-            por="5,99"
-            ou="6,99"
-          />
-          <Card
-            nome="nome do produto"
-            desc="descrição completa do produto aqui "
-            de="10,99"
-            por="5,99"
-            ou="6,99"
-          />
-          <Card
-            nome="nome do produto"
-            desc="descrição completa do produto aqui "
-            de="10,99"
-            por="5,99"
-            ou="6,99"
-          />
-          <Card
-            nome="nome do produto"
-            desc="descrição completa do produto aqui "
-            de="10,99"
-            por="5,99"
-            ou="6,99"
-          />
-          <Card
-            nome="nome do produto"
-            desc="descrição completa do produto aqui "
-            de="10,99"
-            por="5,99"
-            ou="6,99"
-          />
-          <Card
-            nome="nome do produto"
-            desc="descrição completa do produto aqui "
-            de="10,99"
-            por="5,99"
-            ou="6,99"
-          />
-          <Card
-            nome="nome do produto"
-            desc="descrição completa do produto aqui "
-            de="10,99"
-            por="5,99"
-            ou="6,99"
-          />
+          {informacoes.length>0 &&
+            informacoes.map((item,index) => {
+              return (
+                <Card
+                  key={index}
+                  img={item.image}
+                  nome={item.name}
+                  desc={item.description}
+                  de={item.oldPrice}
+                  por={item.price}
+                  vezes={item.installments.count}
+                  ou={item.installments.value}
+                />
+              );
+            })}
         </div>
-        <Button style={S.button} content="Adquira mais produtos aqui" />
+        <Button onClick={nextPage} style={S.button} content="Adquira mais produtos aqui" />
       </section>
       <section className={S.thirdSec}>
         <div className={S.h3Line}>
@@ -93,8 +73,11 @@ const Main = () => {
             <hr />
           </div>
         </div>
-        <p className={S.p}>Quer que seus amigos também ganhem a lista personalizada deles? Preencha agora!</p>
-        <FormBottom/>
+        <p className={S.p}>
+          Quer que seus amigos também ganhem a lista personalizada deles?
+          Preencha agora!
+        </p>
+        <FormBottom />
       </section>
     </main>
   );
